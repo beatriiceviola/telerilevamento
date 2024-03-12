59af8ea9387dd3fe76e506805dc346b47f529b49
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-03-12 17:32:23 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -3,3 +3,17 @@ install.packages("terra")
 
 #per vedere se il pacchetto è stato installato inseriamo
 library(terra)
+
+#install new packages devtools in CRAN
+install.packages("devtools")
+
+#poi controlliamo se devtools è stato installato con
+library(devtools)
+
+# install the imageRy package from GitHub
+#a devtools function
+devtools::install_github("ducciorocchini/imageRy")
+
+
+#controlliamo l'installazione sempre con library
+library(imageRy)
