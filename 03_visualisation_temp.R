00bf485fddf196839975f75deca76341171ef4b3
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-03-14 17:15:09 +0100

    Update 03_visualisation_temp.R

--- 03_visualisation_temp.R
+++ 03_visualisation_temp.R
@@ -8,3 +8,28 @@ im.list()
 
 #Importing data
 mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
+b2<- im.import("sentinel.dolomites.b2.tif" )
+
+#Plotting the data
+plot(mato)
+
+#Cambiamo la scala di colori
+clg<-colorRampPalette(c("black", "grey", "light grey"))(3)
+
+#Plotting the data
+plot(b2, col=clg)
+
+#Cambiamo scala di colori di nuovo
+clch<-colorRampPalette(c("cyan4","magenta","cyan", "chartreuse"))(100)
+
+#Plotting the data
+plot(b2, col= clch)
+
+#Importing the additional bands
+b3 <- im.import("sentinel.dolomites.b3.tif")  
+plot(b3, col=clch)
+b4<-im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=clch)
+b8 <- im.import("sentinel.dolomites.b8.tif")  
+plot(b8, col=clch)
+                         
