a3b4dfcc6c43549935b3f8db9503c5e02939584f
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-03-18 10:40:41 +0100

    Update 03_visualisation_temp.R

--- 03_visualisation_temp.R
+++ 03_visualisation_temp.R
@@ -6,7 +6,7 @@ library(imageRy)
 #List of data avialable in imageRy
 im.list()
 
-#Importing data banda del rosso
+#Importing data banda del blu
 #assegno un oggetto alla mia funzione im.import
 b2<- im.import("sentinel.dolomites.b2.tif" )
 #Plotting the data
@@ -48,10 +48,48 @@ plot(b3, col=clch)
 plot(b4, col=clch)
 plot(b8, col=clch)
 
-#Uniamo le 4 bande in una sola immagine satellitare 
+#Uniamo le 4 bande in una sola immagine satellitare?
 #Prima associamo alla funzione concatenate un oggetto
 stacksent<-c(b2,b3,b4,b8)
 #poi lo plottiamo
 plot(stacksent, col=clch)
 #mettiamo tra parentesi quadre????
 plot(stacksent[[4]], col=clch)
+
+#Se voglio cancellare l'immagine plottata uso la funzione
+dev.off()
+
+#stacksent[[1]]= b2= blue
+#stacksent[[2]]= b3= green
+#stacksent[[3]]= b4= red
+#stacksent[[4]]= b8= nir
+
+#RGB plotting
+
+#im.plotRGB(stacksent, r=3, g=2, b=1 ) plotto l'immagine con colori naturali
+im.plotRGB(stacksent, 3, 2, 1 )
+
+#plottiamo l'immagine con l'infrarosso al posto del rosso
+im.plotRGB(stacksent, 4, 2, 1 )
+
+#multiframe
+par(mfrow=c(1,2))
+im.plotRGB(stacksent, 3, 2, 1 )
+im.plotRGB(stacksent, 4, 2, 1 )
+im.plotRGB(stacksent, 4, 3, 2 )
+
+#nir on green
+im.plotRGB(stacksent, 3, 4, 2 )
+
+#nir on blue
+im.plotRGB(stacksent, 3, 2, 4 )
+
+#Final multiframe
+par(mfrow=c(2,2))
+im.plotRGB(stacksent, 3, 2, 1 )
+im.plotRGB(stacksent, 4, 2, 1 )
+im.plotRGB(stacksent, 3, 4, 2 )
+im.plotRGB(stacksent, 3, 2, 4 )
+
+#correlations of information
+pairs(stacksent)
