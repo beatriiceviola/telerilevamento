79279387c411e0dcb469db7eb9958f59be22bdf9
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-04-04 17:35:21 +0200

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -50,5 +50,32 @@ im.plotRGB(m2006, r=2, g=1, b=3) #2006 on green
 im.plotRGB(m2006, r=2, g=3, b=1) #2006 on blue
 
 
+#Calculating DVI
+dvi1992 = m1992[[1]] - m1992 [[2]]
 
+#plotting the DVI
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black"))(100)
+plot(dvi1992, col= cl)
 
+#2006
+dvi2006 = m2006[[1]] - m2006 [[2]]
+
+#Exercise: plot the dvi1992 beside the dvi2006
+par(mfrow=c(1,2))
+plot(dvi1992, col=cl)
+plot(dvi1006, col=cl)
+
+#stacksent
+stackdvi <- c(dvi1992, dvi2006)
+pairs(stackdvi)
+
+#Normalized DVI
+#essendo un'operazione metto = ma potevoi anche usare l'assegnazione <-
+ndvi1992 = dvi1992/(m1992[[1]]+m1992[[2]])
+
+ndvi2006 = dvi2006/(m2006[[1]]+m2006[[2]])
+
+dev.off()
+par(mfrow= c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
