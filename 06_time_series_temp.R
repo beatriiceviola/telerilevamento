e60fa1c8b11e89826b8c36312d9b698e3727830a
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-04-11 16:36:46 +0200

    Create 06_time_series_temp.R

---
+++ 06_time_series_temp.R
@@ -0,0 +1,33 @@
+#Second method to quantify changes in time
+#The first method was based on classification
+
+library(terra)
+library(imageRy)
+
+im.list()
+
+#import the data
+EN01<- im.import("EN_01.png")
+EN13<- im.import("EN_13.png")
+
+
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
