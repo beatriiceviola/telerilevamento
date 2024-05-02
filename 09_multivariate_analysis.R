4741e68e1bfca3706307329e0ed1758c52c844fc
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-02 17:47:11 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate_analysis.R
+++ 09_multivariate_analysis.R
@@ -23,9 +23,19 @@ im.plotRGB(sentdo, r=4, g=3, b=2)
 #vediamo quanto le barre sono correlate tra loro
 pairs(sentdo)
 
+#PCA
+pcimage <- im.pca(sentdo)
 
+tot <- sum(1487.78706,  536.25767,   66.98454,   29.66159)
+1487.78706*100/tot
 
+#plottiamo questa pca con viridis
 
+vir <- colorRampPalette (viridis (7))(100)
+plot(pcimage, col=vir)
 
+#senza dover usare colorRampPalette si può anche fare
+plot(pcimage, col=viridis(100))
+plot(pcimage, col=plasma(100))
 
 
