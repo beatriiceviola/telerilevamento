db4743d2236f8e1fd1f0d7e996f77cf70899ae86
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-05 15:37:46 +0200

    Update 01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -14,21 +14,29 @@ a+b #esprimiamo la stessa operazione di prima, quindi il risultato è sempre 82
 flowers <- c(3, 6, 8, 10, 15, 18) #se non avessi usato la fuznione c(), R avrebbe assegnato all'oggeto flowers solo il primo numero
 flowers
 
-insects <- c(10,  16, 25, 42, 61, 73)
+insects <- c(10,  16, 25, 42, 61, 73) #altro esempio con la funzione concatenate
 insects
 
+#Ora posso fare un plot(x,y)
 plot (flowers, insects)
 
-#changing plot paramet
-
-#color
+#la funzione plot possiede vari parametri tra cui
+#il colore, posso modificarlo scrivendo all'interno del plot
+#plot(x, y, col=""), all'interno delle virgolette inserisco il nome del colore
+#che desidero tra quelli presenti in R
 
 plot (flowers, insects, col= "darkmagenta")
 
-#symbols
+#poi posso cambiare i simboli usando pch=""
+#tra le virgolette inserisco un numero che corrisponde a un simbolo
+#(per capire che numero usare in base alsimbolo che voglio uso tabelle specifiche)
 plot (flowers, insects, col= "darkmagenta", pch= 24)
 
-#symbol dimensions
+#ancora posso cambiare la dimensione dei simboli
+#con cex="", tra virgolette indico di quanto voglio aumentare o diminuire il simbolo
 plot (flowers, insects, col= "darkmagenta", pch= 24, cex= 2)
 
-
+#infine posso modificare il nome delle mie ascisse e ordinate
+#faccio ciò aggiungendo xlab="" e ylab="" 
+#sempre specificando tra virgolette il nome che voglio usare
+plot (xlab="carbonati", ylab="anidride carbonica", col= "darkmagenta", pch= 24, cex= 2)
