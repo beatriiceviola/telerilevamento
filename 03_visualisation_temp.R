176d378fae1021e1c098426b47369f120c2d5e55
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-05 17:47:25 +0200

    Update 03_visualisation_temp.R

--- 03_visualisation_temp.R
+++ 03_visualisation_temp.R
@@ -64,48 +64,76 @@ plot(b3, col=clch)
 plot(b4, col=clch)
 plot(b8, col=clch)
 
-#Uniamo le 4 bande in una sola immagine satellitare?
-#Prima associamo alla funzione concatenate un oggetto
+#STACKSENT??????
+#Prima creiamo un vettore con le nostre 4 bande grazie alla funzione concatenate
+#e poi lo associamo ad un oggetto
 stacksent<-c(b2,b3,b4,b8)
 #poi lo plottiamo
 plot(stacksent, col=clch)
-#mettiamo tra parentesi quadre????
-plot(stacksent[[4]], col=clch)
+#sebbene sia più rapido del par() usato prima,
+#in questo caso il multiframe è sempre di 2x2 ???????????
 
 #Se voglio cancellare l'immagine plottata uso la funzione
 dev.off()
 
+#Immaginiamo di voler vedere una sola banda, ad esempio la quarta
+#ovvero quella dell'infrarosso
+#Per farlo specifico che dal mio array prendo solo il quarto oggetto 
+#mettendolo in una doppia parentesi quadra
+#doppia perché l'immagine è in 2 dimensioni
+plot(stacksent[[4]], col=clch)
+
+#Creiamoci per comodità un indice che ci dica a quali elementi dello stacksent
+#corrispondono quali bande
 #stacksent[[1]]= b2= blue
 #stacksent[[2]]= b3= green
 #stacksent[[3]]= b4= red
 #stacksent[[4]]= b8= nir
 
 #RGB plotting
+#RGB sono i 3 filtri (red, green, blue)
+#Per plottare un'immagine RGB usiamo la funzione di imageRy
+#im.plotRGB(), all'interno delle parentesi specifico qual è l'oggetto con le mie bande
+#nel nostro caso stacksent,e poi sepcifico su quale filtro voglio mettere quale banda
 
+#Ad esempio 
 #im.plotRGB(stacksent, r=3, g=2, b=1 ) plotto l'immagine con colori naturali
-im.plotRGB(stacksent, 3, 2, 1 )
+#perché so che nel mio stacksent 3 è l abanda del rosso e la lascio sul filtro rosso
+#2 è la banda del verde e la lascio sul filtro verde
+#1 è l abanda blu e la lascio sul filtro blu
+im.plotRGB(stacksent, 3, 2, 1 ) #per semplicità posso anche evitare di scrivere r=, g= e b=
 
 #plottiamo l'immagine con l'infrarosso al posto del rosso
+#nir on red
+#quindi tutto ciò che riflette l'infrarosso, come l avegetazione, ora lo visualizzo rosso
 im.plotRGB(stacksent, 4, 2, 1 )
 
-#multiframe
-par(mfrow=c(1,2))
-im.plotRGB(stacksent, 3, 2, 1 )
-im.plotRGB(stacksent, 4, 2, 1 )
-im.plotRGB(stacksent, 4, 3, 2 )
-
+#oppure con l'infrarosso al posto del verde
 #nir on green
 im.plotRGB(stacksent, 3, 4, 2 )
 
+#o ancora sul blu
 #nir on blue
 im.plotRGB(stacksent, 3, 2, 4 )
 
+
+#multiframe
+par(mfrow=c(1,3))
+im.plotRGB(stacksent, 3, 2, 1 ) #immagine naturale
+im.plotRGB(stacksent, 4, 2, 1 ) #nir on red
+im.plotRGB(stacksent, 4, 3, 2 ) #immagine a falsi colori
+
+dev.off()
+
 #Final multiframe
 par(mfrow=c(2,2))
-im.plotRGB(stacksent, 3, 2, 1 )
-im.plotRGB(stacksent, 4, 2, 1 )
-im.plotRGB(stacksent, 3, 4, 2 )
-im.plotRGB(stacksent, 3, 2, 4 )
+im.plotRGB(stacksent, 3, 2, 1 ) #colori naturali, come li vedrebbero i nostri occhi
+im.plotRGB(stacksent, 4, 2, 1 ) #nir on red
+im.plotRGB(stacksent, 3, 4, 2 ) #nir on green
+im.plotRGB(stacksent, 3, 2, 4 ) #nir on blue
+#notiamo quanto l'infrarosso vicino ci restituisca molta informazione
 
 #correlations of information
 pairs(stacksent)
+#ci fa vedere che l'infrarosso non è correlato alle altre bande, mentre le altre bande sono ben correlate tra loro
+#questo ci fa capire perché l'infrarosso aggiunge informazioni
