55576d5f8d8247371afe21d11ed04f2574693554
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-05 19:06:30 +0200

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -1,46 +1,60 @@
 #Spectral indices
+
+#come sempre richiamiamo i pacchetti che ci servono
 library(terra)
 library(imageRy)
 
-#list of files
+#Poi richiamiamo la lista per vedere le immagini disponibili su imageRy
 im.list()
 
+#Importiamo l'immagine del 1992 di matogrosso con im.import()
 im.import("matogrosso_l5_1992219_lrg.jpg")
-m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg") #sempre associando alla funzione un oggetto
 
-#bands
-# banda 1 = NIR
-# banda 2 = red
-# banda 3 = green
+#Per quest'immagine le bande erano già montate in questo modo
+# banda 1 = nir = R
+# banda 2 = red = G
+# banda 3 = green = B
 
 #plot RGB
 #NIR on red (la vegetazione appare rossa)
 im.plotRGB(m1992, r=1, g=2, b=3)
 
 #NIR on green (la vegetazione appare verde)
-im.plotRGB(m1992,r=2, g=1, b=3)
+im.plotRGB(m1992,r=2, g=1, b=3) #sposto la banda 1 che è la banda nir sul filtro del verde 
+#la parte rosa è il suolo nudo che è poco rispetto alla fortesta, quindi si ha poco impatto umano
 
 #NIR on blue (la vegetazione appare blu)
-im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m1992, r=2, g=3, b=1) #sposto la banda 1 che è la banda nir sul filtro blu
+#in questo modo il suolo nudo diventa giallo il che è più evidente per l'occhio umano
 
-#2006
+
+#matogrosso 2006
+#vediamo se la situazione si è modificata in 14 anni
+#importiamo l'immagine e associamola ad un oggetto
 im.import("matogrosso_ast_2006209_lrg.jpg")
 m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
 
-#mettimao l eimmagini vicine
+#anche per quest'immagine vediamo
+#nir on red
+im.plotRGB(m2006, r=1, g=2, b=3)
+
+#nir on green
+im.plotRGB(m2006, r=2, g=1, b=3)
+
+#nir on blue
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+#mettiamo le immagini nir on red del 1992 e del 2006 vicine con un multiframe
 par(mfrow=c(1,2))
 im.plotRGB(m1992, r=1, g=2, b=3)
 im.plotRGB(m2006, r=1, g=2, b=3)
 
 dev.off()
 
-#NIR ontop of green
-im.plotRGB(m2006, r=2, g=1, b=3)
-
-#NIR ontop of blue
-im.plotRGB(m2006, r=2, g=3, b=1)
-
-#mettiamo tutte e 6 le immagini insieme
+#mettiamo ora tutte e 6 le immagini insieme
+#nella prima riga le 3 immagini del 1992
+#nella seconda riga le 3 immagini del 2006 in modo da apprezzare meglio l'impatto antropico
 par(mfrow=c(2,3))
 im.plotRGB(m1992, r=1, g=2, b=3) #1992 on red
 im.plotRGB(m1992,r=2, g=1, b=3) #1992 on green
