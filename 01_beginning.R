e4444e24342ec9410fa18a74d0ff420560b93c4b
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-05 15:25:19 +0200

    Update 01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -1,14 +1,17 @@
-# First R script
-
-# R as calculator
-a <-6 * 7
+#Prima lezione di telerilevamento
+#Impariamo ad usare R
+#Per prima cosa associamo una funzione (molitplicazione) ad un oggetto (lettera)
+a <- 6*7
 b <- 5*8
 
-a+b
+(6*7)+(5*8) #il risultato è 82, ma possiamo esprimere quest'operazione anche in un altro modo
+a+b #esprimiamo la stessa operazione di prima, quindi il risultato è sempre 82
 
-#Arrays
+#Arrays o vettori, sono serie di oggetti 
+#attraverso la funzione concatenate creiamo un vettore 
+#assegniamo sempre alla funzione un oggetto, flowers in questo caso
 
-flowers <- c(3, 6, 8, 10, 15, 18)
+flowers <- c(3, 6, 8, 10, 15, 18) #se non avessi usato la fuznione c(), R avrebbe assegnato all'oggeto flowers solo il primo numero
 flowers
 
 insects <- c(10,  16, 25, 42, 61, 73)
