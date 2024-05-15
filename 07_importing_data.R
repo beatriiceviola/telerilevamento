166dc043893348171b611be162253385983e699a
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-15 19:51:45 +0200

    Update 07_importing_data.R

--- 07_importing_data.R
+++ 07_importing_data.R
@@ -1,28 +1,49 @@
-#importing data from external sources
+#Importing data from external sources
 
-#ho scaricato il file eclissi.png nella cartella Download del PC
-
+#Come sempre richiamiamo i pacchetti che ci serviranno
 library(terra)
 library(imageRy)
 
-setwd("C:/Users/anisz/Downloads") #spieghiamo al sistema in quale cartella del nostro PC estrarre i dati che vogliamo caricare #wd sta per working directory, molto utilizzato nelle funzioni
-#nb x windows: usare il backslash non va bene ad R, bisogna invertire \ con /
+#Ora spieghiamo al sistema in quale cartella del nostro PC estrarrei dati che vorremo utilizzare
+#setwd sta per set working directory
+#L'argomento di questa funzione è il percorso della cartella
+setwd("/User/") 
+#Nell'argomento vanno sempre le virgolette "" perché usciamo da R
+#Mai usare il backslash \ ma sempre lo slash normale /
 
-#imprting images
-eclissi<-rast("eclissi.png") #la funzione crea degli oggetti delle classi SpatRaster #occhio al nome, serve anche l'estensione
-eclissi #richiama l'immagine per cui ci escono i dati vari
+#Importiamo le immagini con terra
+eclissi <- rast("eclissi.png") #il nome dell'immagine deve sempre essere corretto
+eclissi #richiamandolo vediamo tutti i dati dell'immagine
 
-#plotting data
-im.plotRGB(eclissi, 1, 2, 3) #si vede l'immagine; non sappiamo quali siano le bande ma possiamo invertirle per visualizzare l'immagine nelle varie bande
+#Plottiamo l'immagine con i colori originali
+im.plotRGB(eclissi, 1, 2, 3) 
 
-deforestation<-rast("driversdef.jpg") #cos' importo l'altra immagine
+#Volendo potrei poi anche giocare con le bande
+im.plotRGB(eclissi, 3,2,1)
+im.plotRGB(eclissi, 2,3,1)
 
-#importing images from copernicus
-soil<-rast("soil.nc")
-plot(soil) #così plotto l'immagine
-plot(soil[[1]]) #per plottare solo il primo livello
+# Facciamo la differenza tra due bande 
+dif=eclissi[[1]] - eclissi[[2]]
+plot(dif)
 
-#crop data
-ext<-c(25,30,55,48) #definisco le estensioni xmin xmax, ymin,ymax
-soilcrop<-crop(soil,ext) #ottengo una nuova variabile  #NON VA
-plot(soilcrop[[1]])
+# Importiamo un'altra immagine dall'Earth Observatory
+#Da qui va ancora modificato
+nz <- rast("nz.jpg") # stessa cosa fatta prima???
+plot(nz) # immagine della Nuova Zelanda????
+
+# importiamo il dato scaricato da copernicus sul soil
+
+soil <- rast("c_gls_SSM1km_202404210000_CEURO_S1CSAR_V1.2.1.nc") # sempre il nome dell'immagine di com'è salvata nel pc
+plot(soil)
+plot(soil[[1]]) # solo il primo livello che ci interessa
+
+## possiamo usare le coordinate per ritagliare le immagini e focalizzarci su un area
+
+# prima di tutto si definisce un estensione
+# prima si da la X minima e X massima e la Y min e max
+
+ext <- c(25,30,55,58) # creo il vettore con l'estensione di X (primi due numeri) e Y (terzo e quarto numero)
+soilcrop <- crop(soil, ext) # taglio l'immagine nell'estensione individuata con la funzione crop
+plot(soilcrop) # area d'interessse e errore associato
+plot(soilcrop[[1]]) # solo della prima banda, la nostra variabile d'interesse senza l'errore associato
+# se mantieni sempre le stesse coordinate puoi usarlo anche per diverse immagini sulla stessa area di studio
