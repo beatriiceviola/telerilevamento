5262cbb4c57998892ec3cc4407afbb6650685521
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-16 14:36:05 +0200

    Update 00_exam_project.R

--- 00_exam_project.R
+++ 00_exam_project.R
@@ -10,12 +10,23 @@ library(imageRy) #per la funzione im.classify()
 #Osserviamo gli effetti di questa riforestazione
 #Le immagini sono state prese da Copernicus Browser
 #Iniziamo cambiando la nostra work directoy da cui R prenderà i dati che ci servono
-setwd("/Users/macbookairair/Progetto R")
+setwd("/Users/macbookairair/Downloads/Progetto R")
 
 #Ora andiamo a prendere dalla nostra cartella l'immagine che ci serve
+#Le immagini sono state fatte da Sentine-2
+#Le bande sono già decise
+#Prima importiamole immagini con colori naturali
+#banda 1 = R = red
+#banda 2 = G = green
+#banda 3 = B = blue
 suppressWarnings({
 tc2017 <- rast("tc.sept2017.300m.jpg")
 })
 suppressWarnings({
 tc2023 <- rast("tc.sept2023.300m.jpg")
 })
+
+#Mettiamo le 2 immagini vicine per poter fare un confronto diretto, quindi 1 riga e 2 colonne
+par(mfrow=c(1,2))
+plot(tc2017)
+plot(tc2023)
