beed311231e02e2f6cd7e3805e45d89ef0aa8e6a
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-16 14:28:08 +0200

    Update 00_exam_project.R

--- 00_exam_project.R
+++ 00_exam_project.R
@@ -1,7 +1,21 @@
 #Richiamo i pacchetti che andrò ad utilizzare
-library(terra)
-library(ggplot)
-library(viridis)
-library(imageRy)
+library(terra) #per la funzione rast
+library(ggplot2) #per costruire grafici
+library(viridis) #per avere una colorRampoPalette adatta ai daltonici
+library(imageRy) #per la funzione im.classify()
 
 
+#Il progetto riguarda una zona di riforestazione nei dintorni di Miguel Pereira in Brasile
+#Dopo gli incendi del 2014 sono stati ripiantati più di 60 mila alberi tra il 2018 e il 2021
+#Osserviamo gli effetti di questa riforestazione
+#Le immagini sono state prese da Copernicus Browser
+#Iniziamo cambiando la nostra work directoy da cui R prenderà i dati che ci servono
+setwd("/User/macbookairair/Progetto R")
+
+#Ora andiamo a prendere dalla nostra cartella l'immagine che ci serve
+suppressWarnings({
+tc2017 <- rast("tc.sept2017.300m.jpg")
+})
+suppressWarnings({
+tc2023 <- rast("tc.sept2023.300m.jpg")
+})
