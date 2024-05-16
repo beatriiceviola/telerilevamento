9d3de6b03673f375d4f9d3dbd8daf2226858ec24
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2024-05-16 15:13:29 +0200

    Update 00_exam_project.R

--- 00_exam_project.R
+++ 00_exam_project.R
@@ -12,6 +12,9 @@ library(imageRy) #per la funzione im.classify()
 #Iniziamo cambiando la nostra work directoy da cui R prenderà i dati che ci servono
 setwd("/Users/macbookairair/Downloads/Progetto R")
 
+#Scegliamo una colorRampPalette inclusiva per i daltonici
+cl<-colorRampPalette(viridis(7))(255) 
+
 #Ora andiamo a prendere dalla nostra cartella l'immagine che ci serve
 #Le immagini sono state fatte da Sentinel-2
 #Le bande sono già decise
@@ -19,10 +22,6 @@ setwd("/Users/macbookairair/Downloads/Progetto R")
 #banda 1 = R = red
 #banda 2 = G = green
 #banda 3 = B = blue
-
-#Scegliamo una colorRampPalette inclusiva per i daltonici
-cl<-colorRampPalette(viridis(7))(255) 
-
 suppressWarnings({
 tc2017 <- rast("tc.sept2017.300m.jpg")
 })
@@ -32,5 +31,28 @@ tc2023 <- rast("tc.sept2023.300m.jpg")
 
 #Mettiamo le 2 immagini vicine per poter fare un confronto diretto, quindi 1 riga e 2 colonne
 par(mfrow=c(1,2))
-plot(tc2017, col=cl)
-plot(tc2023, col=cl)
+plot(tc2017)
+plot(tc2023)
+dev.off()
+
+#Importiamo ora le stesse immagini con falsi colori
+#sempre bande già decise da Sentinel-2
+#banda 1 = R = NIR
+#banda 2 = G = red
+#banda 3 = B = green
+suppressWarnings({
+fc2017 <- rast("fc.sep2017.jpg")
+}) #NON VA DA RIGUARDARE
+suppressWarnings({
+fc2023 <- rast("fc.sep2023.jpg")
+})
+
+#Mettiamole vicine
+par(mfrow=c(1,2))
+plot(fc2017)
+plot(fc2023)
+dev.off()
+
+#Calcoliamo il Difference Vegetation Index normalizzato , NDVI che varia da 1 a -1
+#NDVI= (NIR-red)/(NIR+red)
+NDVI2017 <- (fc2017[[1]]-fc2023[[2]])/(fc2017[[1]+fc2023[[2]])
